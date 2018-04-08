b380817ea9ad818b40a4a29a2b1811fd234edda6
shahidtufail <37308955+shahidtufail@users.noreply.github.com>
2018-04-08 09:05:08 +0530

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,7 @@
+
+
+source("LData.R")
+png(filename = "Plot1.png", width = 480, height = 480, units = "px")
+hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", breaks = 12, ylim = c(0, 1200))
+dev.off()
+rm(data)
